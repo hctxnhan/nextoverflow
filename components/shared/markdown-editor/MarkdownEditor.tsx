607feee43f3c9f5b133dc3e3f25644c8db2ea499
Thanh Nhan <hctxnhan@gmail.com>
2023-10-16 17:56:57 +0700

    Change Markdown editor setting

--- components/shared/markdown-editor/MarkdownEditor.tsx
+++ components/shared/markdown-editor/MarkdownEditor.tsx
@@ -1,14 +1,41 @@
 "use client";
 
-import MDEditor, { MDEditorProps, RefMDEditor } from "@uiw/react-md-editor";
+import MDEditor, {
+  EditorContext,
+  MDEditorProps,
+  RefMDEditor,
+} from "@uiw/react-md-editor";
 import { useTheme } from "next-themes";
-import { RefAttributes } from "react";
+import { RefAttributes, useContext } from "react";
 import rehypeSanitize from "rehype-sanitize";
+import { Button } from "@/components/ui/button";
 
 import "@uiw/react-markdown-preview/markdown.css";
 import "@uiw/react-md-editor/markdown-editor.css";
 
 type MarkdownEditorProps = MDEditorProps & RefAttributes<RefMDEditor>;
+
+function ChangeModeButton() {
+  const { preview, dispatch } = useContext(EditorContext);
+  function handleChangeMode() {
+    dispatch?.({
+      preview: preview === "edit" ? "preview" : "edit",
+    });
+  }
+
+  if (preview === "edit") {
+    return (
+      <Button type="button" onClick={handleChangeMode}>
+        Preview
+      </Button>
+    );
+  }
+  return (
+    <Button type="button" onClick={handleChangeMode}>
+      Continue editing
+    </Button>
+  );
+}
 
 export function MarkdownEditor(props: MarkdownEditorProps) {
   const { theme } = useTheme();
@@ -16,11 +43,20 @@ export function MarkdownEditor(props: MarkdownEditorProps) {
   return (
     <MDEditor
       {...props}
+      preview="edit"
       previewOptions={{
         rehypePlugins: [[rehypeSanitize]],
       }}
       data-color-mode={theme as "light" | "dark" | undefined}
       id="markdown-editor"
+      extraCommands={[
+        {
+          name: "preview",
+          keyCommand: "preview",
+          value: "preview",
+          icon: <ChangeModeButton />,
+        },
+      ]}
     />
   );
 }
